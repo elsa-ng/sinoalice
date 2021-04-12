816d03f48ce8a19ec43901c46c3867431698329b
Elsa Ng <elsa.ng@ontario.ca>
2021-04-12 04:30:18 -0400

    Added logo to landing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static(path.join(__dirname, "/static")));
+app.use(express.static('static'));
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
