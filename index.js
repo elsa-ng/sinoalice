731e8819f8364dc03eba4010f6daac875f7f00eb
Elsa Ng <elsa.ng@ontario.ca>
2021-04-13 03:44:11 -0400

    migrated landing page to use exphbs

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-require ("dotenv").config();
-var express = require("express");
-var path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+const path = require("path");
+const exphbs = require('express-handlebars');
+const helpers = require('./lib/exphbs_helpers');
 
-var app = express();
-var HTTP_PORT = process.env.PORT || 8080;
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+dotenv.config();
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+        section: helpers.header_section
+    }
+}));
+app.set('view engine', '.hbs');
 app.use(express.static('static'));
 
 // setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/landing.html"));
+app.get("/", function (req, res) {
+    res.render('landing', {
+        data: null,
+        layout: 'main'
+    });
+});
+
+app.get("/viewData", function (req, res) {
+
+    var someData = [{
+        name: "John",
+        age: 23,
+        occupation: "developer",
+        company: "Scotiabank",
+        isVisible: false,
+        isContract: false
+    },{
+        name: "Sarah",
+        age: 32,
+        occupation: "manager",
+        company: "TD",
+        isVisible: true,
+        isContract: true
+    }];
+
+    res.render('viewData', {
+        data: someData,
+        layout: false
+    });
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/about.html"));
+    res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
 app.use((req, res) => {
